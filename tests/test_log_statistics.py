81d7beee57f64644248e1544a92fd9a08ae5f727
riley206 <rileymaltos@gmail.com>
2024-01-05 12:48:41 -0800

    removed import

--- tests/test_log_statistics.py
+++ tests/test_log_statistics.py
@@ -30,7 +30,6 @@ from mock import MagicMock
 from volttron.client.messaging.health import STATUS_GOOD
 from volttron.client.vip.agent import Agent
 from volttrontesting.fixtures.volttron_platform_fixtures import volttron_instance
-#from volttron.client import get_ops, get_home
 
 test_config = {
     "analysis_interval_sec": 2,
